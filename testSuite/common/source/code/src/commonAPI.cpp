eebf41d2b2c65f90757e7847547ad757b894f5a6
tberi <tberi@tberis-mbp.mmx400r>
2013-12-01 21:32:11 +0530

    Fixed Mac OSX linker error Undefined symbols for architecture x86_64 "std::ctype<char>::_M_widen_init() const. This error started appearing after gcc is upgraded to version 4.8.1. This error probably comes because CUDA libraries have been compiled with a different version of gcc and two libstdc++ are getting linked.

--- testSuite/common/source/code/src/commonAPI.cpp
+++ testSuite/common/source/code/src/commonAPI.cpp
@@ -57,8 +57,7 @@ void RegisterLibraryCallback(int pParallelMode, std::string pCallbackKey, pmCall
 
     static const char* lArray[6] = {"1", "2", "3", "4", "5", "6"};
 
-    std::string lTempKey;
-    lTempKey = pCallbackKey + lArray[pParallelMode - 1];
+    std::string lTempKey(pCallbackKey + lArray[pParallelMode - 1]);
 
     SAFE_PM_EXEC( pmRegisterCallbacks((char*)lTempKey.c_str(), pCallbacks, pCallbackHandle) );
 }
