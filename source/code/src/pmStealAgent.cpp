53bd01186b4eb79b6001b1d8442624482c1ef545
tberi <tberi@tberiMacBookPro.local>
2015-10-19 22:47:29 +0530

    Comment typo correction

--- source/code/src/pmStealAgent.cpp
+++ source/code/src/pmStealAgent.cpp
@@ -217,7 +217,7 @@ void pmStealAgent::RecordSubtaskSubscriptionFetchTime(pmExecutionStub* pStub, do
     FINALIZE_RESOURCE_PTR(dStubLock, RESOURCE_LOCK_IMPLEMENTATION_CLASS, &lDynamicAggressionStubData.stubLock, Lock(), Unlock());
 
     /* There is a loss in pre-fetching when a subtask is stolen (even when pipeline continuation is done.
-       This is because there is no subtask to start start pre-fetching data for before steal actually completes.
+       This is because there is no subtask to start pre-fetching data for before steal actually completes.
        By stealing early, we hope to cover this loss.
        The subscription fetch time sent to this function is the time a subtask waits on its subscriptions
        and all the time overlapped in pre-fetch is not accounted. For this reason, we consider maximum pre-fetch
